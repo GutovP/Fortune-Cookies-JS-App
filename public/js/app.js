b300324e792d2527ea7c49e7b026930ac2a858c8
Petar Gutov <pgutov@gmail.com>
2021-01-20 19:04:17 +0100

    add routes, setup system.js

---
+++ public/js/app.js
@@ -0,0 +1,14 @@
+import 'jquery';
+
+import { MyRouter } from 'myRouter';
+
+const router = new MyRouter();
+
+router
+  .on('/', () => (location.hash = '#/home'))
+  .on('/home', homeController)
+  .on('/home/:category', someController)
+  .on('/my-cookie', someController);
+
+$(window).on('load', () => router.navigate());
+$(window).on('hashchange', () => router.navigate());
---
+++ public/js/system-config.js
@@ -0,0 +1,20 @@
+System.config({
+  transpiler: 'plugin-babel',
+  map: {
+    // System.js files
+    'plugin-babel': 'libs/systemjs-plugin-babel/plugin-babel.js',
+    'systemjs-babel-build':
+      'libs/systemjs-plugin-babel/systemjs-babel-browser.js',
+
+    // App files
+    app: 'js/app.js',
+    myRouter: 'js/myRouter.js',
+
+    // templates
+
+    // Library files
+    jquery: 'libs/jquery/dist/jquery.min.js',
+  },
+});
+
+System.import('app.js');
